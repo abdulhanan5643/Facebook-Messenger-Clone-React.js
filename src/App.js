56b0f599a3c0fd10ad176e613b7e2f6b1c250dad
abdulhanan5643 <59803552+abdulhanan5643@users.noreply.github.com>
2020-11-16 13:43:47 +0500

    material ui used

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {Button, FormControl, Input, InputLabel} from "@material-ui/core";
 import './App.css';
 
 function App() {
@@ -8,16 +9,25 @@ function App() {
 
     console.log(messages);
     const sendMessage = (event)=>{
+        event.preventDefault();
         setMessages([...messages,input]);
         setInput('');
     }
 
     return <div className="App">
-        <h1>Messenger</h1>
-        <input value={input} onChange={event => setInput(event.target.value)}/>
-        <button onClick={sendMessage}>Send Message</button>
 
-        {messages.map(message => <p>{message}</p>)}
+        <h1>Hey Mate! 😃</h1>
+        <form>
+            <FormControl>
+                <InputLabel>Enter a message...</InputLabel>
+                <Input value={input} onChange={event => setInput(event.target.value)}/>
+                <Button disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}>Send
+                    Message</Button>
+            </FormControl>
+        </form>
+
+
+        {messages.map(message => <p key={message}>{message}</p>)}
 
     </div>
 }
