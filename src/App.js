a883209824eab509399976b14a6ad13ef2fd9018
abdulhanan5643 <59803552+abdulhanan5643@users.noreply.github.com>
2020-11-19 19:03:33 +0500

    Send button moved to bottom and styled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import {Button, FormControl, Input, InputLabel} from "@material-ui/core";
+import {FormControl, Input, InputLabel} from "@material-ui/core";
 import Message from "./Message";
 import './App.css';
 import db from "./firebase";
 import firebase from "firebase";
 import FlipMove from "react-flip-move";
+import SendIcon from '@material-ui/icons/Send';
+import { IconButton } from '@material-ui/core';
 
 
 function App() {
@@ -35,15 +37,17 @@ function App() {
 
     return <div className="App">
 
+        <img src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100" alt="Messenger Logo" />
         <h1>Messenger 💬</h1>
         <h2>Welcome {username} 😃</h2>
-        <form>
+        <form className="app_form">
             <FormControl>
                 <div>
                     <InputLabel>Enter a message...</InputLabel>
                     <Input value={input} onChange={event => setInput(event.target.value)}/>{"\n"}
-                    <Button disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}>Send
-                        Message</Button>
+                    <IconButton disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}>
+                        <SendIcon />
+                    </IconButton>
                 </div>
             </FormControl>
         </form>
